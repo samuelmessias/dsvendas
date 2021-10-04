9889f26aa4a4edb8beb7f2250a4ab6b986371ae7
Samuel <samuelmessias6@gmail.com>
2021-10-04 15:37:08 -0300

    Custom constraintValidator

--- backend/src/main/java/com/dvsuperior/dscatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/dvsuperior/dscatalog/dto/UserInsertDTO.java
@@ -1,5 +1,8 @@
 package com.dvsuperior.dscatalog.dto;
 
+import com.dvsuperior.dscatalog.services.validation.UserInsertValid;
+
+@UserInsertValid
 public class UserInsertDTO extends UserDTO{
 	
 	private static final long serialVersionUID = 1L;
