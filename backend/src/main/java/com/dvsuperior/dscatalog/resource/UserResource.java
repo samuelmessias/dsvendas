8e7adfee0ccd1b2e55c2af76a02ea9a574a57ebc
Samuel <samuelmessias6@gmail.com>
2021-10-04 16:08:30 -0300

    UserUpdateValidator

--- backend/src/main/java/com/dvsuperior/dscatalog/resource/UserResource.java
+++ backend/src/main/java/com/dvsuperior/dscatalog/resource/UserResource.java
@@ -21,6 +21,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.dvsuperior.dscatalog.dto.UserDTO;
 import com.dvsuperior.dscatalog.dto.UserInsertDTO;
+import com.dvsuperior.dscatalog.dto.UserUpdateDTO;
 import com.dvsuperior.dscatalog.services.UserService;
 
 @RestController
@@ -53,9 +54,9 @@ public class UserResource {
 	}
 	
 	@PutMapping(value = "/{id}")
-	public ResponseEntity<UserDTO> update(@PathVariable Long id, @Valid @RequestBody UserDTO dto){
-		dto = service.update(id, dto);
-		return ResponseEntity.ok().body(dto);
+	public ResponseEntity<UserDTO> update(@PathVariable Long id, @Valid @RequestBody UserUpdateDTO dto){
+		UserDTO newDto = service.update(id, dto);
+		return ResponseEntity.ok().body(newDto);
 	}
 	
 	@DeleteMapping(value = "/{id}")
--- backend/src/main/java/com/dvsuperior/dscatalog/services/UserService.java
+++ backend/src/main/java/com/dvsuperior/dscatalog/services/UserService.java
@@ -16,6 +16,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.dvsuperior.dscatalog.dto.RoleDTO;
 import com.dvsuperior.dscatalog.dto.UserDTO;
 import com.dvsuperior.dscatalog.dto.UserInsertDTO;
+import com.dvsuperior.dscatalog.dto.UserUpdateDTO;
 import com.dvsuperior.dscatalog.entities.Role;
 import com.dvsuperior.dscatalog.entities.User;
 import com.dvsuperior.dscatalog.repositories.RoleRepository;
@@ -60,7 +61,7 @@ public class UserService {
 	
 
 	@Transactional
-	public UserDTO update(Long id, UserDTO dto) {
+	public UserDTO update(Long id, UserUpdateDTO dto) {
 		try {
 			User entity = repository.getOne(id);
 			copyDtoToentity(dto, entity);
