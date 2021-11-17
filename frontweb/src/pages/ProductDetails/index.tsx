92f1bc8134ec015300c6fe759c9cbbee11945283
Samuel <samuelmessias6@gmail.com>
2021-11-17 14:36:50 -0300

    catalog - ProducDetails limks

--- frontweb/src/pages/ProductDetails/index.tsx
+++ frontweb/src/pages/ProductDetails/index.tsx
@@ -1,5 +1,6 @@
 import { ReactComponent as ArrowIcon } from 'assets/image/arrow.svg';
 import ProductPrice from 'components/ProductPrice';
+import { Link } from 'react-router-dom';
 
 import './styles.css';
 
@@ -7,24 +8,35 @@ const ProductDetails = () => {
   return (
     <div className="product-details-container">
       <div className="base-card product-details-card">
-        <div className="goback-container">
-          <ArrowIcon />
-          <h2>Voltar</h2>
-        </div>
+        <Link to="/products">
+          <div className="goback-container">
+            <ArrowIcon />
+            <h2>Voltar</h2>
+          </div>
+        </Link>
         <div className="row">
           <div className="col-xl-6">
             <div className="img-container">
-                <img src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg" alt="Nome product" />
+              <img
+                src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg"
+                alt="Nome product"
+              />
             </div>
             <div className="name-price-container">
-                <h1>Nome do produto</h1>
-                <ProductPrice price={2345.67} />
+              <h1>Nome do produto</h1>
+              <ProductPrice price={2345.67} />
             </div>
           </div>
           <div className="col-xl-6">
             <div className="description-container">
-                <h2>Descrição produto</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit voluptatibus, ratione iste cumque fugit quas accusantium culpa quae, sapiente deserunt minima, adipisci dignissimos voluptas eaque. Suscipit, necessitatibus iste! Cum, aliquam?</p>
+              <h2>Descrição produto</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Reprehenderit voluptatibus, ratione iste cumque fugit quas
+                accusantium culpa quae, sapiente deserunt minima, adipisci
+                dignissimos voluptas eaque. Suscipit, necessitatibus iste! Cum,
+                aliquam?
+              </p>
             </div>
           </div>
         </div>
